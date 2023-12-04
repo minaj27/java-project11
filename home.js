f42b5726d2059c392b4c00f1fd57151355026e87
minaj27 <minaj13m@gmail.com>
2023-12-04 23:54:52 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,43 @@
+var mainDiv = document.getElementById("main"); 
+
+var  c1 = document.createElement("h1");
+c1.innerText = "Fullstack Development";
+
+var c2 = document.createElement("h3");
+c2.innerText = "HTML";
+
+var c3 = document.createElement("h3");
+c3.innerText ="CSS 3"; 
+
+var c4 = document.createElement("h5");
+c4.innerText = "Bootstrap";
+
+var c5 = document.createElement("h5");
+c5.innerText = "MUI";
+
+var c6 = document.createElement("h5");
+c6.innerText = "Basic";
+
+var c7 = document.createElement("h5");
+c7.innerText = "Advance"
+
+var c8 = document.createElement("h3");
+c8.innerText = "JavaScript"
+
+var c9 = document.createElement("h5")
+c9.innerText = "TypeScript"
+
+var c10 = document.createElement("h5");
+c10.innerText = "EcmaScript"
+
+mainDiv.appendChild(c1);
+mainDiv.appendChild(c2);
+mainDiv.appendChild(c6);
+mainDiv.appendChild(c7);
+mainDiv.appendChild(c3);
+mainDiv.appendChild(c4);
+mainDiv.appendChild(c5);
+mainDiv.appendChild(c8);
+mainDiv.appendChild(c9);
+mainDiv.appendChild(c10);
+
